125bd3d8ddff11dae7f4780111a1e3d5e9a90c80
Ghassen-bgh <ghassen.boughammoura@gmail.com>
2021-09-30 13:54:09 +0100

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 int main(void)
-	{
-		char A [] = "\"Programming is like building a multilingual puzzle";
-		puts(A);
-		return 0;
-	}
+{
+char A[] = "\"Programming is like building a multilingual puzzle";
+
+puts(A);
+return (0);
+}
