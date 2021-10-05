10314f747f34f2e213ac74acb80de2c8afdee759
Ghassen-bgh <ghassen.boughammoura@gmail.com>
2021-10-05 14:31:45 +0100

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include"main.h"
+/**
+ * print_alphabet_x10 - print lowercase alphabet x10
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char alphabet = 'a';
+	int i;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for(alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		{
+			_putchar(alphabet);
+		}
+		_putchar('\n');
+	}
+}
