9a5df59618b7a205003f912f651238099d10b2d3
Ghassen-bgh <ghassen.boughammoura@gmail.com>
2021-10-05 21:11:30 +0100

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,16 +9,10 @@ int print_last_digit(int n)
 	int l;
 
 	if (n < 0)
-	{
-	n *= -1;
+		n *= -1;
+
 	l = n % 10;
 	_putchar('0' + l);
 	return (l);
-	}
-	else
-	{
-		l = n % 10;
-		_putchar('0' + l);
-	return (l);
-	}
+
 }
