25e96f7494c79546254eab10059a6fa3b0ae0d42
Ghassen-bgh <3591@holbertonschool.com>
2021-10-14 20:22:04 +0100

    task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @s: string
+ * Return: string with all letters capitalized
+ */
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
+	}
+	return (s);
+}
