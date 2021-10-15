df8ee27268bc128a33f31cf0685a92f01c426272
Ghassen-bgh <3591@holbertonschool.com>
2021-10-15 20:49:03 +0100

    task 9

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,21 @@
+#include"main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: integer to print
+ */
+
+void print_number(int n)
+{
+	int x;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = n * -1;
+	}
+	x = n;
+	if (x / 10)
+		print_number(x / 10);
+	_putchar(x % 10 + '0');
+}
