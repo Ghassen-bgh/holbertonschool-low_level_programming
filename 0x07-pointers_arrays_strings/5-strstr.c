a27d9349707bddcf396013702035aa3688fcb250
Ghassen-bgh <3591@holbertonschool.com>
2021-10-18 18:47:30 +0100

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include"main.h"
+/**
+ * _strstr - locate and return pointer to first occurence of substring
+ * @haystack: string to search
+ * @needle: target substring to search for
+ * Return: pointer to index of string at first occurence of whole substring
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int index;
+
+	if (*needle == 0)
+		return (haystack);
+	while (*haystack)
+	{
+		index = 0;
+		if (haystack[index] == needle[index])
+		{
+			do {
+				if (needle[index + 1] == '\0')
+					return (haystack);
+				index++;
+			} while (haystack[index] == needle[index]);
+		}
+		haystack++;
+	}
+	return ('\0');
+}
