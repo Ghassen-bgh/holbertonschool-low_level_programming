75cda1a48730a8623ce1bc8b5daa94a728c5f6df
Ghassen-bgh <3591@holbertonschool.com>
2021-10-18 11:46:45 +0100

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include"main.h"
+/**
+ * _memcpy - copy n bytes of memory from source to destination.
+ *
+ * @dest: destination.
+ * @src: source.
+ * @n: number of bytes.
+ *
+ * Return: pointer to dest.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int s = n;
+
+	if (s > 0)
+	{
+		int i;
+
+		for (i = 0; i < s; i++)
+			dest[i] = src[i];
+	}
+	return (dest);
+}
