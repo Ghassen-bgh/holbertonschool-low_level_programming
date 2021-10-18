16c078d114411c1e9269fa46c16785a579ff9931
Ghassen-bgh <3591@holbertonschool.com>
2021-10-18 18:54:31 +0100

    chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include"main.h"
+/**
+ * print_chessboard - print chessboard 
+ * @a: the row of the array
+ */
+
+void print_chessboard(char (*a)[8])
+{
+
+	int row;
+	int column;
+
+	for (row = 0; row < 8; row++)
+	{
+		for (column = 0; column < 8; column++)
+			_putchar(a[row][column]);
+		_putchar('\n');
+	}
+}
