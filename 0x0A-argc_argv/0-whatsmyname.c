a5ca276973f2047ea0c62d657fcbf8ec2018ceed
Ghassen-bgh <3591@holbertonschool.com>
2021-10-22 13:25:27 +0100

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+* main - prints the programs name.
+* @argc: The size of the argv array.
+* @argv: An array of size argc.
+* Return: 0
+*/
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
