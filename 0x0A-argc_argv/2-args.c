787e20ca0925081e28c6fe80dc60bcb17628baa0
Ghassen-bgh <3591@holbertonschool.com>
2021-10-22 14:30:38 +0100

    print arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+* main - prints the programs name.
+* @argc: The size of the argv array.
+* @argv: An array of size argc.
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
