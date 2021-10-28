cf89515f7b7f310b4b8c5e5f3a93457fb526b8f9
Ghassen-bgh <3591@holbertonschool.com>
2021-10-28 16:00:15 +0100

    concatenate

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,9 +17,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	while (!s1[i] == '\0')
+	while (s1[i])
 		i++;
-	while (!s2[j] == '\0')
+	while (s2[j])
 		j++;
 	if (n >= j)
 	{
