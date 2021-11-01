9b59ed843abde41d566800edb54a81fa925ee05d
Ghassen-bgh <3591@holbertonschool.com>
2021-11-01 15:02:43 +0000

    Structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure for dogs
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner name of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif
