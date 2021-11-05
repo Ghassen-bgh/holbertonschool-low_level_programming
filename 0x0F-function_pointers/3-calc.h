d1b74595e5c2c2f388711b2b4211b91197936423
Ghassen-bgh <3591@holbertonschool.com>
2021-11-05 17:03:34 +0100

    print header

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -12,8 +12,8 @@ int (*get_op_func(char *s))(int, int);
  */
 typedef struct op
 {
-    char *op;
-    int (*f)(int a, int b);
+	char *op;
+	int (*f)(int a, int b);
 } op_t;
 
 int op_add(int a, int b);
