caba574cd148ebff69ebb22fc6fbcde1bb3486b5
Ghassen-bgh <3591@holbertonschool.com>
2021-11-05 12:11:55 +0100

    hello world asm

--- 0x10-variadic_functions/100-hello_world.asm
+++ 0x10-variadic_functions/100-hello_world.asm
@@ -4,7 +4,7 @@
         extern	printf		; the C function, to be called
 
         section .data		; Data section, initialized variables
-msg:	db "Hello, world", 0	; C string needs 0
+msg:	db "Hello, World", 0	; C string needs 0
 fmt:    db "%s", 10, 0          ; The printf format, "\n",'0'
 
         section .text           ; Code section.
