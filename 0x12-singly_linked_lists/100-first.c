41361e1794490875eabe5a1ee2041b0bd628da34
Ghassen-bgh <3591@holbertonschool.com>
2021-11-11 23:19:34 +0100

    First

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * myStartupFun - Function that runs before main
+ */
+void __attribute__ ((constructor)) myStartupFun(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
